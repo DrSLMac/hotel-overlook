8b451c363b99ee321c151f69d286ad43c37a02a1
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2022-08-03 13:20:22 -0500

    create class and test files

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,35 @@
+import './css/styles.css';
+import './images/favicon.ico';
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+
+// function postApiData(url, data) {
+//     fetch(url, {
+//         method: 'POST', headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(data)
+//     }).then(data => data.json()).then(data => {
+//         console.log(data)
+//         fetch(url)
+//             .then(data => data.json())
+//             .then(data => console.log(data))
+//     })
+//         .catch(error => console.log(error));
+// }
+
+// function fetchWords() {
+//     return fetch('http://localhost:3001/api/v1/words')
+//     .then(response => response.json())
+//       .then((data) => {
+//         data.forEach(word => words.push(word))
+//         setGame()
+//       })
+//       .catch(error => console.log(error))
+//     }
